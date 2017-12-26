29de83dab56215bdf1293ffec9ec4261ad52f09e
kacper <kacper.szatan@gmail.com>
2017-12-26 21:16:12 +0100

    Basic dumb board display (without connection between nodes)

--- ChineseCheckers/src/main/java/com/checkers/server/board/Board.java
+++ ChineseCheckers/src/main/java/com/checkers/server/board/Board.java
@@ -2,7 +2,7 @@ package com.checkers.server.board;
 
 public class Board {
     int[] fieldsPerRow;
-    int[][] board;
+    int[][] tempBoard;
     int[][] graph;
     int players;
     int pieces;
@@ -18,14 +18,61 @@ public class Board {
         } else {
             setBoardFields(arguments);
             displayboard(fieldsPerRow);
+            createTempBoard();
            // createBoard();
             //fillPlayers();
         }
     }
 
+    private int sumFields(){
+        int sum = 0;
+        for(int i = 0; i < fieldsPerRow.length; i++){
+            sum = sum + fieldsPerRow[i];
+        }
+        return sum;
+    }
+
+    private void createTempBoard(){
+        this.tempBoard = new int[(this.cornerWidth*4 + 1) + 2][((this.cornerWidth*3 + 1)*2 - 1) + 2];  //int[rows][columns]
+        prefillTempBoard((this.cornerWidth*4 + 1) + 2, ((this.cornerWidth*3 + 1)*2 - 1) + 2);
+        fillTempBoard((this.cornerWidth*4 + 1) + 2, ((this.cornerWidth*3 + 1)*2 - 1) + 2);
+        displayboard2(tempBoard,(this.cornerWidth*4 + 1) + 2, ((this.cornerWidth*3 + 1)*2 - 1) + 2);
+    }
+
+    private void fillTempBoard(int height, int length){
+        for(int i = 1; i < height - 1; i++){
+            int fields = fieldsPerRow[i - 1];
+            int verticalCenter = length/2 + 1;
+            int start = verticalCenter - fields;
+            for(int j=0;j<fields;j++){
+                this.tempBoard[i][start] = 0;
+                start = start + 2;
+            }
+        }
+    }
+
+
+
     public void displayboard(int[] board){              //  temporary to display board
         for(int i = 0; i < board.length; i++)           //  for development usage
             System.out.println(board[i]);               //  need to be deleted in final version
+    }
+
+    public void displayboard2(int[][] board, int height, int length){            //  temporary to display board
+        for(int i = 0; i < height; i++) {
+            for (int j = 0; j < length; j++)                                    //  for development usage
+                System.out.print(board[i][j]);                                       //  need to be deleted in final version
+            System.out.print("\n");
+        }
+    }
+
+
+    private void prefillTempBoard(int height, int length){
+        for(int i=0;i<height;i++){
+            for(int j=0;j<length;j++){
+                this.tempBoard[i][j]=-1;
+            }
+        }
     }
 
     private void setBoardFields(String[] arguments){
@@ -33,10 +80,10 @@ public class Board {
         pieces = Integer.parseInt(arguments[3]);
         corners = Integer.parseInt(arguments[4]);
         cornerWidth = Integer.parseInt(arguments[5]);
-        setFieldsPerRow(cornerWidth);
+        setFieldsPerRow();
     }
 
-    private void setFieldsPerRow(int cornerWidth){
+    private void setFieldsPerRow(){
         int height = cornerWidth*4 + 1;
         this.fieldsPerRow = new int[height];
         int fields = 0;
